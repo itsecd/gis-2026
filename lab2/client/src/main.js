da1d49a668ee67f1b65752f203d60f3a87a18eaa
Белякова Вероника <113890061+Cat-sandwich@users.noreply.github.com>
2026-03-16 09:26:57 +0400

    Выполнена лабораторная работа 2

---
+++ lab2/client/src/main.js
@@ -0,0 +1,65 @@
+import "ol/ol.css";
+
+import Map from "ol/Map";
+import View from "ol/View";
+
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+
+import ImageLayer from "ol/layer/Image";
+import ImageWMS from "ol/source/ImageWMS";
+
+import { fromLonLat } from "ol/proj";
+
+const map = new Map({
+  target: "map",
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: fromLonLat([50.412987, 53.118023]),
+    zoom: 16,
+  }),
+});
+
+const buildingsLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: "http://localhost:8080/geoserver/gis/wms",
+    params: {
+      LAYERS: "gis:buildings",
+      TILED: true,
+    },
+    ratio: 1,
+    serverType: "geoserver",
+  }),
+});
+
+const roadsLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: "http://localhost:8080/geoserver/gis/wms",
+    params: {
+      LAYERS: "gis:roads",
+      TILED: true,
+    },
+    ratio: 1,
+    serverType: "geoserver",
+  }),
+});
+
+const poiLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: "http://localhost:8080/geoserver/gis/wms",
+    params: {
+      LAYERS: "gis:poi",
+      TILED: true,
+    },
+    ratio: 1,
+    serverType: "geoserver",
+  }),
+});
+
+map.addLayer(buildingsLayer);
+map.addLayer(roadsLayer);
+map.addLayer(poiLayer);
