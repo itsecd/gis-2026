d655df7fc6bed235e3cab9416a7a13bc2f718422
THO LE LOC <leloctho2307@gmail.com>
2026-03-20 03:54:21 +0400

    add lab 2

---
+++ lab2-geoserver/client/src/main.js
@@ -0,0 +1,121 @@
+// import './style.css'
+// import javascriptLogo from './assets/javascript.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import { setupCounter } from './counter.js'
+
+// document.querySelector('#app').innerHTML = `
+// <section id="center">
+//   <div class="hero">
+//     <img src="${heroImg}" class="base" width="170" height="179">
+//     <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
+//     <img src=${viteLogo} class="vite" alt="Vite logo" />
+//   </div>
+//   <div>
+//     <h1>Get started</h1>
+//     <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
+//   </div>
+//   <button id="counter" type="button" class="counter"></button>
+// </section>
+
+// <div class="ticks"></div>
+
+// <section id="next-steps">
+//   <div id="docs">
+//     <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
+//     <h2>Documentation</h2>
+//     <p>Your questions, answered</p>
+//     <ul>
+//       <li>
+//         <a href="https://vite.dev/" target="_blank">
+//           <img class="logo" src=${viteLogo} alt="" />
+//           Explore Vite
+//         </a>
+//       </li>
+//       <li>
+//         <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//           <img class="button-icon" src="${javascriptLogo}" alt="">
+//           Learn more
+//         </a>
+//       </li>
+//     </ul>
+//   </div>
+//   <div id="social">
+//     <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
+//     <h2>Connect with us</h2>
+//     <p>Join the Vite community</p>
+//     <ul>
+//       <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
+//       <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
+//       <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
+//       <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
+//     </ul>
+//   </div>
+// </section>
+
+// <div class="ticks"></div>
+// <section id="spacer"></section>
+// `
+
+// setupCounter(document.querySelector('#counter'))
+
+
+import 'ol/ol.css';
+import './style.css';
+
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import ImageLayer from 'ol/layer/Image';
+import OSM from 'ol/source/OSM';
+import ImageWMS from 'ol/source/ImageWMS';
+import { fromLonLat } from 'ol/proj';
+
+const osmLayer = new TileLayer({
+  source: new OSM()
+});
+
+const buildingsLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/gis/wms',
+    params: {
+      LAYERS: 'gis:buildings',
+      TILED: true
+    },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+});
+
+const roadsLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/gis/wms',
+    params: {
+      LAYERS: 'gis:roads',
+      TILED: true
+    },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+});
+
+const poiLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/gis/wms',
+    params: {
+      LAYERS: 'gis:poi',
+      TILED: true
+    },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+});
+
+const map = new Map({
+  target: 'map',
+  layers: [osmLayer, buildingsLayer, roadsLayer, poiLayer],
+  view: new View({
+    center: fromLonLat([106.68, 10.77]),
+    zoom: 15
+  })
+});
