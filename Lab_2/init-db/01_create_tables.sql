32b97dcacd075e1584d1488903d13099e50d449f
UselessMiva <ivnickaba@gmail.com>
2026-03-22 17:35:28 +0400

    fix

--- Lab_2/init-db/01_create_tables.sql
+++ Lab_2/init-db/01_create_tables.sql
@@ -1,42 +1,43 @@
-
+-- Включение расширения PostGIS
 CREATE EXTENSION IF NOT EXISTS postgis;
 
-
 -- Здания (полигональные объекты)
-DROP TABLE IF EXISTS buildings;
+DROP TABLE IF EXISTS buildings CASCADE;
 CREATE TABLE buildings (
     id SERIAL PRIMARY KEY,
-    osm_id BIGINT,
-    name VARCHAR(255),
-    building VARCHAR(100),
-    amenity VARCHAR(100),
-    geom geometry(MultiPolygon, 4326)
+    osm_id TEXT UNIQUE,
+    addr_housenumber TEXT,
+    addr_street TEXT,
+    addr_village TEXT,
+    addr_region TEXT,
+    building TEXT,
+    building_levels TEXT,
+    geom GEOMETRY(MultiPolygon, 4326) NOT NULL
 );
 
 -- Дороги (линейные объекты)
-DROP TABLE IF EXISTS roads;
+DROP TABLE IF EXISTS roads CASCADE;
 CREATE TABLE roads (
     id SERIAL PRIMARY KEY,
-    osm_id BIGINT,
-    name VARCHAR(255),
-    highway VARCHAR(100),
-    surface VARCHAR(100),
-    geom geometry(MultiLineString, 4326)
+    osm_id TEXT UNIQUE,
+    name TEXT,
+    highway TEXT,
+    surface TEXT,
+    geom GEOMETRY(MultiLineString, 4326) NOT NULL
 );
 
--- Точки интереса (точечные объекты)
-DROP TABLE IF EXISTS pois;
-CREATE TABLE pois (
+-- POI (точечные объекты)
+DROP TABLE IF EXISTS poi CASCADE;
+CREATE TABLE poi (
     id SERIAL PRIMARY KEY,
-    osm_id BIGINT,
-    name VARCHAR(255),
-    amenity VARCHAR(100),
-    shop VARCHAR(100),
-    geom geometry(Point, 4326)
+    osm_id TEXT UNIQUE,
+    name TEXT,
+    category TEXT,
+    geom GEOMETRY(Point, 4326)
 );
 
--- Пространственные индексы 
+-- ===================== Пространственные индексы =====================
 
 CREATE INDEX idx_buildings_geom ON buildings USING GIST (geom);
 CREATE INDEX idx_roads_geom ON roads USING GIST (geom);
-CREATE INDEX idx_pois_geom ON pois USING GIST (geom);
+CREATE INDEX idx_poi_geom ON poi USING GIST (geom);
