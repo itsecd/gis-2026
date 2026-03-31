40856ae349cd543d76ddb1cd38dd6e65c0192159
Daryaskhrv <daryasheqer@yandex.ru>
2026-03-31 19:25:50 +0400

    Исправлены координаты, слои перенесены, gitignore изменен

--- lab2/client/src/main.js
+++ lab2/client/src/main.js
@@ -4,62 +4,54 @@ import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import ImageLayer from 'ol/layer/Image';
 import ImageWMS from 'ol/source/ImageWMS';
+import { fromLonLat } from 'ol/proj';
 
-// Создаем базовую карту с подложкой OpenStreetMap
+
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM()
+    }),
+    new ImageLayer({
+      source: new ImageWMS({
+        url: "http://localhost:8080/geoserver/gis/wms",
+        params: {
+          LAYERS: "gis:buildings",
+          TILED: true,
+          TRANSPARENT: true
+        },
+        ratio: 1,
+        serverType: "geoserver",
+      }),
+    }),
+    new ImageLayer({
+      source: new ImageWMS({
+        url: "http://localhost:8080/geoserver/gis/wms",
+        params: {
+          LAYERS: "gis:roads",
+          TILED: true,
+          TRANSPARENT: true
+        },
+        ratio: 1,
+        serverType: "geoserver",
+      }),
+    }),
+    new ImageLayer({
+      source: new ImageWMS({
+        url: "http://localhost:8080/geoserver/gis/wms",
+        params: {
+          LAYERS: "gis:poi",
+          TILED: true,
+          TRANSPARENT: true
+        },
+        ratio: 1,
+        serverType: "geoserver",
+      }),
     })
   ],
   view: new View({
-    // Координаты центра - нужно подобрать под твой район
-    // Это примерно для Самары/Тольятти
-    center: [5486325, 7082910],
+    center: fromLonLat([49.101301, 53.247287]),
     zoom: 16
   })
 });
-
-const buildingsLayer = new ImageLayer({
-  source: new ImageWMS({
-    url: "http://localhost:8080/geoserver/gis/wms",
-    params: {
-      LAYERS: "gis:buildings",
-      TILED: true,
-      TRANSPARENT: true, // добавляем прозрачность
-    },
-    ratio: 1,
-    serverType: "geoserver",
-  }),
-});
-
-const roadsLayer = new ImageLayer({
-  source: new ImageWMS({
-    url: "http://localhost:8080/geoserver/gis/wms",
-    params: {
-      LAYERS: "gis:roads",
-      TILED: true,
-      TRANSPARENT: true,
-    },
-    ratio: 1,
-    serverType: "geoserver",
-  }),
-});
-
-const poiLayer = new ImageLayer({
-  source: new ImageWMS({
-    url: "http://localhost:8080/geoserver/gis/wms",
-    params: {
-      LAYERS: "gis:poi",
-      TILED: true,
-      TRANSPARENT: true,
-    },
-    ratio: 1,
-    serverType: "geoserver",
-  }),
-});
-
-map.addLayer(buildingsLayer);
-map.addLayer(roadsLayer);
-map.addLayer(poiLayer);
