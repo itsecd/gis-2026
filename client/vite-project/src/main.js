3d5b686637409b1c835b4a8fba213e2f4a13266e
dedrowe <barushnikovvs@gmail.com>
2026-04-05 18:08:45 +0400

    complete lab2

---
+++ client/vite-project/src/main.js
@@ -0,0 +1,40 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import ImageLayer from 'ol/layer/Image';
+import ImageWMS from 'ol/source/ImageWMS';
+
+document.body.style.margin = '0';
+
+const mapElement = document.getElementById('map');
+mapElement.style.width = '100vw';
+mapElement.style.height = '100vh';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+const wmsLayer = new ImageLayer({
+  source: new ImageWMS({
+    url: 'http://localhost:8080/geoserver/gis/wms',
+    params: {
+      LAYERS: 'gis:buildings',
+      TILED: true
+    },
+    ratio: 1,
+    serverType: 'geoserver'
+  })
+});
+
+map.addLayer(wmsLayer);
